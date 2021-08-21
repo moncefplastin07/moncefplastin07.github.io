5956246fc427c4a9dde317b698956d75ef86628f
Moncef Gaha <moncefplastin07@gmail.com>
2021-08-22 00:14:00 +0100

    add front page content

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,13 +2,41 @@ import Link from 'next/link'
 import Layout from '../components/Layout'
 
 const IndexPage = () => (
-  <Layout title="Home | Next.js + TypeScript Example">
-    <h1>Hello Next.js 👋</h1>
-    <p>
-      <Link href="/about">
-        <a>About</a>
-      </Link>
+  <Layout title="Moncef Gaha">
+    
+    <div className='w-80 m-auto rounded-full mb-10 border-gray-300 relative' >
+      <img src='https://www.gravatar.com/avatar/ea52ed86c481ca2816b8403acdbc9169?s=512' className="w-80 m-auto rounded-full"></img>
+      <p className='absolute -right-24 -bottom-10 transform -rotate-12'>
+      <span className='text-sm rounded-md	bg-opacity-20 bg-red-200 border border-red-300 py-1 px-2'>Hi there 👋</span>
     </p>
+    </div>
+    
+    <div className="text-center">
+      <h1 className='text-5xl font-bold'>Moncef Gaha</h1>
+      <p className="text-gray-400 p-5">
+        <span>
+          <a href="https://github.com/moncefplastin07" className='hover:text-gray-100'>
+            Github
+          </a>
+        </span>
+        <span>{" "}.{" "}</span>
+        <span>
+          <a href="https://twitter.com/moncefplastin07/" className='hover:text-gray-100'>Twitter</a>
+        </span>
+        <span>{" "}.{" "}</span>
+        <span>
+          <a href="https://facebook.com/moncefplastin07/" className='hover:text-gray-100'>Facebook</a>
+        </span>
+        <span>{" "}.{" "}</span>
+        <span>
+          <a href="https://instagram.com/moncefplastin07/" className='hover:text-gray-100'>Instagram</a>
+        </span>
+      </p>
+
+      <p className='text-xl p-4'>Web backend Developer from Biskra/Algeria</p>
+      <p className="text-md p-5">Email: <a href='mailto:x@moncefgaha.me'>x@moncefgaha.me</a></p>
+
+    </div>
   </Layout>
 )
 
