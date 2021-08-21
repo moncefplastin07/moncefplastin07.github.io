6176752abe515a75d59be5fbb11c7ef61d1a1359
Moncef Gaha <moncefplastin07@gmail.com>
2021-08-22 00:14:13 +0100

    fix same styles

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,41 +1,56 @@
-import React, { ReactNode } from 'react'
+import React, { Component } from "react";
+import Head from "next/head";
 import Link from 'next/link'
-import Head from 'next/head'
+import ToggleDisplayThemeButton from "./ToggleDisplayThemeButton";
 
-type Props = {
-  children?: ReactNode
-  title?: string
+class Layout extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { displayTheme: "light" };
+  }
+  componentDidMount() {
+    this.setState({
+      displayTheme: window.localStorage.displayTheme || "light",
+    });
+    document.getElementById('__next').classList.add(window.localStorage.displayTheme || "light")
+  }
+  toggleDisplayTheme() {
+    const newDisplayTheme = this.state.displayTheme == "light"
+      ? "dark"
+      : "light";
+    document.getElementById('__next').classList.replace(this.state.displayTheme,newDisplayTheme)
+    window.localStorage.setItem("displayTheme", newDisplayTheme);
+    return newDisplayTheme;
+  }
+  render() {
+    return (
+      <div className={`h-screen font-mono dark:bg-black dark:text-gray-300 `}>
+        <Head>
+          <title>{this.props.title}</title>
+          <meta name="viewport" content="width=device-width"></meta>
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+        <div className='m-auto max-w-4xl sm:w-full'>
+          <header>
+            <nav>
+              <Link href="/">
+                <a>Home</a>
+              </Link>{' '}
+              |{' '}
+              <Link href="/about">
+                <a>About</a>
+              </Link>
+            </nav>
+          </header>
+          <ToggleDisplayThemeButton
+            onClick={() =>
+              this.setState({ displayTheme: this.toggleDisplayTheme() })}
+            displayTheme={this.state.displayTheme}
+          />
+          {this.props.children}
+        </div>
+      </div>
+    );
+  }
 }
-
-const Layout = ({ children, title = 'This is the default title' }: Props) => (
-  <div>
-    <Head>
-      <title>{title}</title>
-      <meta charSet="utf-8" />
-      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-    </Head>
-    <header>
-      <nav>
-        <Link href="/">
-          <a>Home</a>
-        </Link>{' '}
-        |{' '}
-        <Link href="/about">
-          <a>About</a>
-        </Link>{' '}
-        |{' '}
-        <Link href="/users">
-          <a>Users List</a>
-        </Link>{' '}
-        | <a href="/api/users">Users API</a>
-      </nav>
-    </header>
-    {children}
-    <footer>
-      <hr />
-      <span>I'm here to stay (Footer)</span>
-    </footer>
-  </div>
-)
-
-export default Layout
+export default Layout;
