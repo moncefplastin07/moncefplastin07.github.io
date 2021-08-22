dd84f8a3c8db763c82f04c571bccf085b41c9191
Moncef Gaha <moncefplastin07@gmail.com>
2021-08-22 07:27:53 +0100

    fix z-index

--- components/ToggleDisplayThemeButton.tsx
+++ components/ToggleDisplayThemeButton.tsx
@@ -2,7 +2,7 @@ export default function ToggleDisplayThemeButton(props) {
     return (
       <button
         onClick={props.onClick}
-        className={`text-4xl duration-200 ease-in-out fixed right-1.5	top-1.5	${
+        className={`text-4xl z-50	duration-200 ease-in-out fixed right-1.5	top-1.5	${
           props.displayTheme == "dark" ? "rotate-360" : ""
         }`}
       >
