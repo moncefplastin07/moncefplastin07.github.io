4a7859ff0e8b4d6fb426b194c8f31b82e03e50df
moncefplastin07 <moncefplastin07@gmail.com>
2021-08-22 11:00:55 +0000

    Deploying to gh-pages from @ moncefplastin07/moncefplastin07.github.io@1c9bae5b2da05456cac51943f20a74580d31abb5 🚀

---
+++ _next/static/16tWbHPId_V2IqPOfSWCX/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-614974c2a153655760d5.js"],"/_error":["static/chunks/pages/_error-737a04e9a0da63c9d162.js"],"/about":["static/chunks/pages/about-73c74f3d0f343227b2b5.js"],sortedPages:["/","/_app","/_error","/about"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
--- _next/static/Amj9LAj_s5ShY2Iq_fD6F/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-9a0f185a0b2071b1d0d0.js"],"/_error":["static/chunks/pages/_error-737a04e9a0da63c9d162.js"],"/about":["static/chunks/pages/about-79654a90254afa428a45.js"],sortedPages:["/","/_app","/_error","/about"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
