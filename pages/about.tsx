b966ab7f04fddc7b9043fc0006796c4923e1b94a
Moncef Gaha <moncefplastin07@gmail.com>
2021-08-22 10:54:11 +0100

    add: app padding

--- pages/about.tsx
+++ pages/about.tsx
@@ -3,13 +3,15 @@ import Layout from '../components/Layout'
 
 const AboutPage = () => (
   <Layout title="Moncef Gaha">
-    <h1>About</h1>
-    <p>commeing soon</p>
-    <p>
-      <Link href="/">
-        <a>Go home</a>
-      </Link>
-    </p>
+    <div className='p-5'>
+      <h1>About</h1>
+      <p>commeing soon</p>
+      <p>
+        <Link href="/">
+          <a>Go home</a>
+        </Link>
+      </p>
+    </div>
   </Layout>
 )
 
