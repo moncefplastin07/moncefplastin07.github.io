894e42f8b0f1e1287c62c0552ac0de0dd6a26c08
Moncef Gaha <moncefplastin07@gmail.com>
2021-08-22 10:53:48 +0100

    add: roll animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,26 @@
 module.exports = {
+  mode: 'jit',
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: 'class', // or 'media' or 'class'
-  theme: {
-    extend: {
+  theme: {  
+     extend: {
       screens: {
         'xs': {'max': '639px'},
+      },
+      rotate: {
+        '360': '360deg',
+      },  
+      keyframes: {
+        roll: {
+          '0%': { transform: 'translateX(0) rotate(0deg)' },
+          '25%': { transform: 'translateX(0.1rem) rotate(80deg)' },
+          '50%': { transform: 'translateX(-0.1rem) rotate(-20deg)' },
+          '75%': { transform: 'translateX(0.1rem) rotate(80deg)' },
+          '100%': { transform: 'translateX(0) rotate(0deg)' },
+        }
+      },
+      animation: {
+        roll: 'roll 0.9s '
       },
     },
   },
