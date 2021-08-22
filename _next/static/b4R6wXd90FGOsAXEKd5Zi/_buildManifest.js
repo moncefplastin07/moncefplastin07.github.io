cfd304ff15acc54266fe64dc7956abab7898b96e
moncefplastin07 <moncefplastin07@gmail.com>
2021-08-22 11:13:18 +0000

    Deploying to gh-pages from @ moncefplastin07/moncefplastin07.github.io@1c9bae5b2da05456cac51943f20a74580d31abb5 🚀

--- _next/static/b4R6wXd90FGOsAXEKd5Zi/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-2dac3cda233b34fba401.js"],"/_error":["static/chunks/pages/_error-737a04e9a0da63c9d162.js"],"/about":["static/chunks/pages/about-25d4934002aade700047.js"],sortedPages:["/","/_app","/_error","/about"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
