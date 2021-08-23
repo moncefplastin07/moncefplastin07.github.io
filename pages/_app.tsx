a9aad4fea17c08a122fd91dcdea0d7509940ed89
Moncef Gaha <moncefplastin07@gmail.com>
2021-08-23 10:18:06 +0100

    style: deno fmt

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,7 @@
-import 'tailwindcss/tailwind.css'
+import "tailwindcss/tailwind.css";
 import { AppProps } from "next/app";
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,42 +1,73 @@
-import Link from 'next/link'
-import Layout from '../components/Layout'
+import Link from "next/link";
+import Layout from "../components/Layout";
 
 const IndexPage = () => (
   <Layout title="Moncef Gaha">
-    
-    <div className='w-80 m-auto rounded-full mb-10 border-gray-300 relative' >
-      <img src='https://www.gravatar.com/avatar/ea52ed86c481ca2816b8403acdbc9169?s=512' className="w-80 m-auto rounded-full"></img>
-      <p className='absolute -right-24 -bottom-10 xs:right-0 xs:-bottom-5 transform -rotate-12 rounded-md	bg-opacity-20 bg-red-200 border border-red-300 py-1 px-2 inline-flex'>
-        <span className='relative text-sm p-1 font-bold'>Hi there </span><p className='delay-75	ease-in-out animate-roll'> 👋</p>
+    <div className="w-80 m-auto rounded-full mb-10 border-gray-300 relative">
+      <img
+        src="https://www.gravatar.com/avatar/ea52ed86c481ca2816b8403acdbc9169?s=512"
+        className="w-80 m-auto rounded-full"
+      >
+      </img>
+      <p
+        className="absolute -right-24 -bottom-10 xs:right-0 xs:-bottom-5 transform -rotate-12 rounded-md	bg-opacity-20 bg-red-200 border border-red-300 py-1 px-2 inline-flex"
+      >
+        <span className="relative text-sm p-1 font-bold">Hi there</span>
+        <p className="delay-75	ease-in-out animate-roll">👋</p>
       </p>
     </div>
     <div className="text-center">
-      <h1 className='text-5xl font-bold'>Moncef Gaha</h1>
+      <h1 className="text-5xl font-bold">Moncef Gaha</h1>
       <p className="text-gray-800 p-5">
         <span>
-          <a href="https://github.com/moncefplastin07" className='hover:text-gray-600'>
+          <a
+            href="https://github.com/moncefplastin07"
+            className="hover:text-gray-600"
+          >
             Github
           </a>
         </span>
         <span>{" "}.{" "}</span>
         <span>
-          <a href="https://twitter.com/moncefplastin07/" className='hover:text-gray-600'>Twitter</a>
+          <a
+            href="https://twitter.com/moncefplastin07/"
+            className="hover:text-gray-600"
+          >
+            Twitter
+          </a>
         </span>
         <span>{" "}.{" "}</span>
         <span>
-          <a href="https://facebook.com/moncefplastin07/" className='hover:text-gray-600'>Facebook</a>
+          <a
+            href="https://facebook.com/moncefplastin07/"
+            className="hover:text-gray-600"
+          >
+            Facebook
+          </a>
         </span>
         <span>{" "}.{" "}</span>
         <span>
-          <a href="https://instagram.com/moncefplastin07/" className='hover:text-gray-600'>Instagram</a>
+          <a
+            href="https://instagram.com/moncefplastin07/"
+            className="hover:text-gray-600"
+          >
+            Instagram
+          </a>
         </span>
       </p>
 
-      <p className='text-xl p-4'>Web backend Developer from Biskra/Algeria</p>
-      <p className="text-md p-5">Email: <a href='mailto:x@moncefgaha.me' className='font-bold p-2 rounded-md border border-green-300 bg-green-300 bg-opacity-20'>x@moncefgaha.me</a></p>
-      
+      <p className="text-xl p-4">Web backend Developer from Biskra/Algeria</p>
+      <p className="text-md p-5">
+        Email:{" "}
+        <a
+          href="mailto:x@moncefgaha.me"
+          className="font-bold p-2 rounded-md border border-green-300 bg-green-300 bg-opacity-20"
+        >
+          x@moncefgaha.me
+        </a>
+      </p>
     </div>
   </Layout>
-)
+);
 
-export default IndexPage
+export default IndexPage;
