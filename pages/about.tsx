553952b4286a1595bac794ea61ebb095e37e081f
Moncef Gaha <moncefplastin07@gmail.com>
2021-08-23 10:42:10 +0100

    add line spaceing

--- pages/about.tsx
+++ pages/about.tsx
@@ -3,32 +3,32 @@ import Layout from "../components/Layout";
 
 const AboutPage = () => (
   <Layout title="Moncef Gaha">
-    <div className="p-5">
-      <p>
+    <div className="px-5">
+      <p className='leading-loose py-7'>
         I am Moncef Gaha,{" "}
         {((Date.now() - new Date("2000-12-07").getTime()) / 1000 / 60 / 60 /
           24 / 30 / 12).toFixed(0)}{" "}
-        year from Algeria, Web backend devloper i started self-learning since 2015, I interested in open source software, I have the skills below
+        year from Algeria, Web backend devloper i started self-learning since 2015, I interested in open source software, I have the skills <a href='#My-skills'>below</a>
       </p>
       <p>
-        <strong>My skills:</strong>
-      </p>
-      <ul className="pl-10 list-disc text-justify">
-        <li className="py-1">🌐 Web Back-end developper</li>
-        <li className="py-1">👨‍💻 I Code with JS/TS and PHP</li>
-        <li className="py-1">🦕 Deno/Node JS developer</li>
+        <strong className='text-lg' id='My-skills'>My skills:</strong>
+      </p >
+      <ul className="pl-10 list-disc text-justify leading-loose">
+        <li>🌐 Web Back-end developper</li>
+        <li>👨‍💻 I Code with JS/TS and PHP</li>
+        <li>🦕 Deno/Node JS developer</li>
       </ul>
       <p>
-        <strong>What I do:</strong>
+        <strong className='text-lg'>What I do:</strong>
       </p>
-      <ul className="pl-10 list-disc text-justify">
-        <li className="py-1">📰 Self-learning to develop my programming skills
+      <ul className="pl-10 list-disc text-justify leading-loose">
+        <li>📰 Self-learning to develop my programming skills
         </li>
-        <li className="py-1">
+        <li>
           ⚡ I like to constantly read technical articles, especially programming
           issues, to find out what&#39;s good
         </li>
-        <li className="py-1">
+        <li>
         💻 Tech content creator for social media platforms (sometimes comic)</li>
       </ul>
       <p className="pt-5">
