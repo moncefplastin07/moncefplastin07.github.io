7a32595f6d6922f6b13af08c5ae333556929079b
Moncef Gaha <moncefplastin07@gmail.com>
2021-08-23 09:43:50 +0100

    add: transition duration to dark/light mode toggling

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -30,7 +30,7 @@ class Layout extends Component<MyProps, MyState> {
   }
   render() {
     return (
-      <div className={`min-h-screen	w-full font-mono dark:bg-black dark:text-gray-300 `}>
+      <div className={`min-h-screen	w-full font-mono transition-colors duration-500	 ease-linear dark:bg-black dark:text-gray-300 `}>
         <Head>
           <title>{this.props.title}</title>
           <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
