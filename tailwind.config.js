aa5944bffd4b940303bb17b7d958bdf4fd411075
moncefplastin07 <moncefplastin07@gmail.com>
2021-10-13 20:19:26 +0100

    feat: add skeleton keyframes and animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -17,10 +17,15 @@ module.exports = {
           '50%': { transform: 'translateX(-0.1rem) rotate(-20deg)' },
           '75%': { transform: 'translateX(0.1rem) rotate(80deg)' },
           '100%': { transform: 'translateX(0) rotate(0deg)' },
+        },
+        skeleton: {
+          '0%': { backgroundColor: 'hsl(200, 20%, 70%)' },
+          '100%': { backgroundColor: 'hsl(200, 20%, 95%)' },
         }
       },
       animation: {
-        roll: 'roll 0.9s '
+        roll: 'roll 0.9s ',
+        skeleton: "skeleton 1s linear infinite alternate"
       },
     },
   },
